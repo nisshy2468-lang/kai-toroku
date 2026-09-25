473603b05af9b249e92d9e82c0530d79a2743de1
nisshy2468-lang <nisshy2468-lang@users.noreply.github.com>
2026-09-26 01:04:09 +0900

    回数台帳アプリ（インストール対応版）

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE='kai-toroku-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}).then(function(){return self.skipWaiting()}));
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(k){
+    return Promise.all(k.filter(function(x){return x!==CACHE}).map(function(x){return caches.delete(x)}));
+  }).then(function(){return self.clients.claim()}));
+});
+self.addEventListener('fetch',function(e){
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(function(r){
+    return r || fetch(e.request).then(function(res){
+      var cp=res.clone();
+      caches.open(CACHE).then(function(c){c.put(e.request,cp)});
+      return res;
+    }).catch(function(){return caches.match('./index.html')});
+  }));
+});
